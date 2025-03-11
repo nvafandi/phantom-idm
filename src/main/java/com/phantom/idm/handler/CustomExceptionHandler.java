83224060e576b8362a89ded20cbdfaf0a7ac5e5c
Nurvan Afandi <nurvan.afandi@gmail.com>
2025-03-11 17:43:55 +0700

    change string utils to objects

--- src/main/java/com/phantom/idm/handler/CustomExceptionHandler.java
+++ src/main/java/com/phantom/idm/handler/CustomExceptionHandler.java
@@ -1,7 +1,6 @@
 package com.phantom.idm.handler;
 
 import com.phantom.idm.dto.response.BaseResponse;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,6 +8,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Objects;
 
 @RestControllerAdvice
 public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
@@ -19,7 +20,7 @@ public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
     public ResponseEntity<Object> handleDataNotFoundException(DataNotFoundException ex) {
         BaseResponse<Object> baseResponse = new BaseResponse<>();
         baseResponse.setStatus(HttpStatus.NOT_FOUND.value());
-        baseResponse.setMessage(StringUtils.isBlank(ex.getMessage()) ? "Data not found" : ex.getMessage());
+        baseResponse.setMessage(Objects.isNull(ex.getMessage()) ? "Data not found" : ex.getMessage());
 
         return new ResponseEntity<>(baseResponse, HttpStatus.NOT_ACCEPTABLE);
     }
@@ -30,7 +31,7 @@ public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
     public ResponseEntity<Object> handleBadRequestException(BadRequestException ex) {
         BaseResponse<Object> baseResponse = new BaseResponse<>();
         baseResponse.setStatus(HttpStatus.BAD_REQUEST.value());
-        baseResponse.setMessage(StringUtils.isBlank(ex.getMessage()) ? "Request failed" : ex.getMessage());
+        baseResponse.setMessage(Objects.isNull(ex.getMessage()) ? "Request failed" : ex.getMessage());
 
         return new ResponseEntity<>(baseResponse, HttpStatus.OK);
     }
@@ -41,7 +42,7 @@ public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
     public ResponseEntity<Object> handleUnauthorizedException(UnauthorizedException ex) {
         BaseResponse<Object> baseResponse = new BaseResponse<>();
         baseResponse.setStatus(HttpStatus.UNAUTHORIZED.value());
-        baseResponse.setMessage(StringUtils.isBlank(ex.getMessage()) ? "Token invalid" : ex.getMessage());
+        baseResponse.setMessage(Objects.isNull(ex.getMessage()) ? "Token invalid" : ex.getMessage());
         return new ResponseEntity<>(baseResponse, HttpStatus.UNAUTHORIZED);
     }
 
